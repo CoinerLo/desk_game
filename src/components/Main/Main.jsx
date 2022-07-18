8b57a24928986502e905cea0911ee594b14c2b5d
CoinerLo <coiner4677@gmail.com>
2022-07-18 17:03:14 +0300

    add ModalStartGame component v0.1.8

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import ModalStartGame from '../ModalStartGame';
 import TableGames from '../TableGames';
 import TableOnlineUsers from '../TableOnlineUsers';
 import styles from './main.module.scss';
 
 const Main = ({ isAuth, usersOnline, gamesList }) => {
   const [ isOpenAside, setIsOpenAside ] = useState(true);
+  const [ isOpenStartGame, setIsOpenStartGame ] = useState(false);
 
   return (
     <section className={styles.main}>
@@ -14,6 +16,13 @@ const Main = ({ isAuth, usersOnline, gamesList }) => {
           <h2>Здесь будет основной контент для всех пользователей, но что бы кнопка что-то показала - нужно залогиниться!</h2>
           <p>Наслаждайтесь!</p>
         </div>
+        <button
+          type='button'
+          className={styles.main_content_buttonStart}
+          onClick={() => setIsOpenStartGame(true)}
+        >
+          Новое сражение
+        </button>
       </div>
       <div className={styles.main_aside__button}>
         <button onClick={() => setIsOpenAside(!isOpenAside)}>O</button>
@@ -24,6 +33,7 @@ const Main = ({ isAuth, usersOnline, gamesList }) => {
           <TableGames gamesList={gamesList} />
         </aside>
       )}
+      {isOpenStartGame && <ModalStartGame setIsOpenStartGame={setIsOpenStartGame} />}
     </section>
   );
 }
---
+++ src/components/ModalStartGame/index.jsx
@@ -0,0 +1,82 @@
+import { useForm } from 'react-hook-form';
+import styles from './modalStartGame.module.scss';
+
+const ModalStartGame = ({ setIsOpenStartGame }) => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      radio: '2'
+    }
+  });
+
+  const sendFormData = (data) => {
+    const { radio } = data;
+    console.log(radio);
+  }
+
+  return (
+    <div className={styles.modalStartGame}>
+      <div className={styles.modalStartGame_content}>
+        <button
+          className={styles.modalStartGame_close}
+          onClick={()=> setIsOpenStartGame(false)}
+        >X
+        </button>
+        <h1>Выберите опции для старта</h1>
+        <form
+          onSubmit={handleSubmit(sendFormData)}
+          className={styles.modalStartGame_form}
+        >
+          <fieldset className={styles.modalStartGame_form__numberOfPlayers}>
+            <legend>Выберите колличество противников</legend>
+            <div className={styles.modalStartGame_form__numberOfPlayers_inp}>
+              <div>
+                <input
+                  type='radio'
+                  {...register("radio")}
+                  id='onePlayer'
+                  value='2'
+                />
+                <label htmlFor='onePleay'>Один противник</label>
+              </div>
+              <div>
+                <input
+                  type='radio'
+                  {...register("radio")}
+                  id='twoPlayer'
+                  value='3'
+                />
+                <label htmlFor='twoPlayer'>Два противника</label>
+              </div>
+              <div>
+                <input
+                  type='radio'
+                  {...register("radio")}
+                  id='threePlayer' 
+                  value='4'
+                />
+                <label htmlFor='threePlayer'>Три противника</label>
+              </div>
+              <div>
+                <input
+                  type='radio'
+                  {...register("radio")}
+                  id='fourPlayer'
+                  value='5'
+                />
+                <label htmlFor='fourPlayer'>Четыре противника</label>
+              </div>
+            </div>
+            <button
+              type='submit'
+              className={styles.modalStartGame_content_start}
+            >
+              В атаку!
+            </button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalStartGame;
